02bb6e0f8f44ce61fac247639a69fb80ba3d86a0
noozip2241993 <70178957+noozip2241993@users.noreply.github.com>
2020-10-14 23:40:58 -0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+import random
+
+for i in range(6):
+    num = random.randrange(1,101)
+    def prime():
+        if num > 1:
+            for i in range(2,num):
+                if(num%i)== 0:
+                    print(num,"is not a prime number")
+                    break
+            else:
+                    print(num,"is a prime number")
+        else:
+            print(num,"is not a prime number")
+    prime()
+   

534b9099979c132216cd38783ae684f6a9543d26
noozip2241993 <70178957+noozip2241993@users.noreply.github.com>
2020-10-15 15:58:42 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,10 +1,12 @@
 import random
 def generating_two_integers():
+    """Generating randomly two positive one-digit integers and return values as a tuple"""
     num1= random.randrange(10)
     num2= random.randrange(10)
-    return two_integers(num1,num2)
+    return two_integers(num1,num2) #pack two integers'value into a tuple
 
 def two_integers(num1,num2):
+    """Checking guess with the answer"""
     answer = num1*num2 
     while True:
         guess = int(input(f'How much is {num1} times {num2}? '))

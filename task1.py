cbdca0ccb419603de3f3f64a4fed987752008074
noozip2241993 <70178957+noozip2241993@users.noreply.github.com>
2020-10-15 17:23:23 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,9 +1,8 @@
 import random
-
-for i in range(6):
-    num = random.randrange(1,101)
-    def prime():
-        """Checking whether num is a prime or not"""
+def generating_defining_prime_number():
+    """Checking whether the argument is a prime number or not"""
+    for i in range(6): #generating six random numbers
+        num = random.randrange(1,101)
         if num > 1: #prime numbers are greater than 1
             for i in range(2,num): #check for factors
                 if(num%i)== 0:
@@ -14,5 +13,4 @@ for i in range(6):
         else:
             print(num,"is not a prime number")
             #if input is less than or equal to 1, it is not a prime number
-    prime()
-   
+generating_defining_prime_number()

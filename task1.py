1d878f61d832ef701c8097630c90b9c1f4eabc48
noozip2241993 <70178957+noozip2241993@users.noreply.github.com>
2020-10-15 17:28:13 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 import random
 def generating_defining_prime_number():
     """Checking whether the argument is a prime number or not"""
-    for i in range(6): #generating six random numbers
+    for j in range(6): #generating six random numbers
         num = random.randrange(1,101)
         if num > 1: #prime numbers are greater than 1
             for i in range(2,num): #check for factors
